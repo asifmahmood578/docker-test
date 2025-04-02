c793a9d934caac20182e12cc7e4c2f5ea60961e4
Asif Mahmood <asifmahmood578@gmail.com>
2025-04-02 17:09:35 +0530

    demo-app-1

---
+++ src/main/java/com/asif/DokerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.asif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asif/DokerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
